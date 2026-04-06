3c2cbecb428817a9b39f4c2b8565c4ca6016c2ba
Bryan Herrera <bryan.herrera72@gmail.com>
2026-04-06 12:28:32 -0700

    added mocking section with notes

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -1,4 +1,4 @@
-package mocking
+package main
 
 import (
 	"fmt"
@@ -8,6 +8,8 @@ import (
 
 const finalWord =  "Go!"
 const countdownStart = 3
+const write = "write"
+const sleep = "sleep"
 
 type Sleeper interface {
 	Sleep()
@@ -17,20 +19,46 @@ type SpySleeper struct{
 	Calls int
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
 func (s *SpySleeper) Sleep(){
 	s.Calls++
 }
 
-type DefaultSleeper struct{}
 
-func (d *DefaultSleeper) Sleep(){
-	time.Sleep(1 * time.Second)
+func(s *SpyCountdownOperations) Sleep(){
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error){
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type ConfigurableSleeper struct{
+	duration time.Duration
+	sleep func(time.Duration)
+}
+
+type SpyTime struct{
+	durationSlept time.Duration
+}
+func (c *ConfigurableSleeper) Sleep(){
+	c.sleep(c.duration)
+}
+
+
+func (s *SpyTime) SetDurationSlept(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 func Countdown(out io.Writer, sleeper Sleeper){
-	for i := countdownStart; i> 0; i--{
+	for i := countdownStart; i > 0; i--{
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
